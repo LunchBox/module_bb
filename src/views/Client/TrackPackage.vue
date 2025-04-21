d1c6931877c2a354a9c9986afdf021811974056d
daniel cheang <daniel@cpttm.org.mo>
2025-04-22 01:10:24 +0800

    finish tracking package page

--- src/views/Client/TrackPackage.vue
+++ src/views/Client/TrackPackage.vue
@@ -1,3 +1,47 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { usePackages } from "@/composables/usePackages";
+
+const { getPackageDetails } = usePackages();
+
+const route = useRoute();
+
+const details = ref(null);
+
+async function load() {
+  console.log(route.params);
+  const data = await getPackageDetails(route.params.trackingNumber);
+  console.log(data);
+  details.value = data;
+}
+
+load();
+</script>
 <template>
   <div>Client Track Packages view</div>
+
+  <div v-if="details">
+    <div>
+      Tracking
+      {{ details.tracking_number }}
+    </div>
+    <div>
+      {{ details.status }}
+      {{ details.from_campus }} -> {{ details.to_campus }}
+    </div>
+    <div>
+      Progress
+      <div v-for="prog in details.progress">
+        <div>
+          {{ prog.status }}
+        </div>
+        <div>
+          {{ prog.datetime }}
+
+          <span v-if="prog.returnning === 1">Returnning</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
